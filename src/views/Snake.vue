5c58d291cc672b3b0addeb9530093a811703fde1
ReinisV <reinis.vesers@gmail.com>
2020-12-10 18:40:46 +0200

    started work

---
+++ src/views/Snake.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="text">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Snake',
+  data() {
+    return {
+      color: 'red'
+    }
+  }
+})
+</script>
+
+<style vars="{ color }">
+.text {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 10px;
+  height: 10px;
+  color: var(--color);
+}
+</style>
