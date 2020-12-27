4f8ee454c428ce8145b3c1dc21b97a460d453c69
ReinisV <reinis.vesers@gmail.com>
2020-12-27 12:24:40 +0200

    updated deploy script

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -4,6 +4,7 @@ const execa = require('execa');
 (async() => {
   try {
     await execa('git', ['checkout', '--orphan', 'gh-pages']);
+
     console.log('Building started...');
     await execa('npm', ['run', 'build']);
 
@@ -14,8 +15,9 @@ const execa = require('execa');
     console.log('Pushing to gh-pages...');
     await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
     await execa('rm', ['-r', folderName]);
-    await execa('git', ['checkout', '-f', 'master']);
+    await execa('git', ['checkout', '-f', 'main']);
     await execa('git', ['branch', '-D', 'gh-pages']);
+
     console.log('Successfully deployed, check your settings');
   } catch (e) {
     console.log(e.message);
