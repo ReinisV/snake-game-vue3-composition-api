0a8e19d22e7ace0b6dadfd47e463e32a0fdeb1df
Reinis Vesers <reinis.vesers@gmail.com>
2021-03-07 17:47:55 +0200

    gh-pages

---
+++ precache-manifest.d671c197bbaeaaf37d17e056c04de993.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877d00ac13b8d8105389",
+    "url": "/snake-game-vue3-composition-api/css/app.fb192d7b.css"
+  },
+  {
+    "revision": "bd57d3c4c0524c8169c56fdca592ea2a",
+    "url": "/snake-game-vue3-composition-api/index.html"
+  },
+  {
+    "revision": "877d00ac13b8d8105389",
+    "url": "/snake-game-vue3-composition-api/js/app.f4dc6017.js"
+  },
+  {
+    "revision": "094ed2da11f0b3d42012",
+    "url": "/snake-game-vue3-composition-api/js/chunk-2f0d9578.c5ce69c4.js"
+  },
+  {
+    "revision": "cd74baab0609f29cf6b7",
+    "url": "/snake-game-vue3-composition-api/js/chunk-6d2165f8.d5afc7ef.js"
+  },
+  {
+    "revision": "e26be24a81e73dd71a01",
+    "url": "/snake-game-vue3-composition-api/js/chunk-vendors.e26c7d6c.js"
+  },
+  {
+    "revision": "e459d11579644c92ddd9726486f60737",
+    "url": "/snake-game-vue3-composition-api/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/snake-game-vue3-composition-api/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/snake-game-vue3-composition-api/precache-manifest.d671c197bbaeaaf37d17e056c04de993.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "hello-vue3"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
